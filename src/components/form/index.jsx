34102e1c0875f6702daf6f47e855bf4df3188065
isanaid <eizisana@gmail.com>
2022-07-16 17:04:19 +0700

    add form edit and preview

---
+++ src/components/form/index.jsx
@@ -0,0 +1,42 @@
+import ReactMarkdown from "react-markdown";
+
+const Form = ({ activeNote, onUpdateNote }) => {
+  const onEditField = (field, value) => {
+    onUpdateNote({
+      ...activeNote,
+      [field]: value,
+      lastModified: Date.now(),
+    });
+  };
+
+  if (!activeNote) return <div className="no-active-note">No Selected Note</div>;
+
+  return (
+    <div className="container-fluid">
+      <div className="note-edit">
+        <input
+          type="text"
+          id="title"
+          placeholder="Note Title"
+          value={activeNote.title}
+          onChange={(e) => onEditField("title", e.target.value)}
+          autoFocus
+        />
+        <textarea
+          id="body"
+          placeholder="Write your note here..."
+          value={activeNote.body}
+          onChange={(e) => onEditField("body", e.target.value)}
+        />
+      </div>
+      <div className="note-preview">
+        <h1 className="preview-title">{activeNote.title}</h1>
+        <ReactMarkdown className="markdown-preview">
+          {activeNote.body}
+        </ReactMarkdown>
+      </div>
+    </div>
+  );
+};
+
+export default Form;
